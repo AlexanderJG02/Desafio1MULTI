4597dda9bb46ff686c5ef12894533ae058d05dfa
ALEXG2 <alexanderj0038@gmail.com>
2024-03-10 00:32:14 -0600

    Cambios Completados

--- desafioo1/componets/Principal.jsx
+++ desafioo1/componets/Principal.jsx
@@ -1,28 +1,26 @@
 import React from "react";
+import { data } from "../src/app/data";
 
-import {data} from "../src/app/data";
-
-export const Principal=()=>{
-    return(
-        <div clasName="container">
-            {data.map(gadgets =>(
-                <div clasName="item" key={gadgets.id} >
-                
-                    <div className="informacion" >
-                        <h2 className="nombre" >{gadgets.nombre}</h2>
-                        <img src={gadgets.imagen} key={gadgets.nombre}/>
-                        <h3>Descripcion</h3>
-                        <p className="descripcion" >{gadgets.descripcion}</p>
-                        <p className="marca" >{gadgets.marca} </p>
-                        <p className="modelo" >{gadgets.modelo}</p>
-                        <p className="precio">{gadgets.precio}</p>
-                        
-                        <p>Caracteristicas</p>
-                        <p className="caracteristicas" >{gadgets.caracteristicas}</p>
-                        <hr></hr>
-                        </div>
-                        </div>
-                                    ))}
+export const Principal = () => {
+  return (
+    <div className="container">
+      {data.map((gadgets) => (
+        <div className="item" key={gadgets.id}>
+          <div className="informacion">
+            <h2 className="nombre">{gadgets.nombre}</h2>
+            <h4 className="descripcion">Descripcion</h4>
+            <p className="descripcion">{gadgets.descripcion}</p>
+            <p className="marca">{gadgets.marca}</p>
+            <p className="modelo">{gadgets.modelo}</p>
+            <p className="precio">{gadgets.precio}</p>
+            <h4>Caracteristicas</h4>
+            <p className="caracteristicas">{gadgets.caracteristicas}</p>
+          </div>
+          <div className="imagen">
+            <img src={gadgets.imagen} key={gadgets.nombre} />
+          </div>
         </div>
-    );
-            }
+      ))}
+    </div>
+  );
+};
